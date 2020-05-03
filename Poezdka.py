32c074946671bf9f84b7be3f3f57a3d9a1d14dc9
DavidTsyganov <60532779+DavidTsyganov@users.noreply.github.com>
2020-05-04 00:30:09 +0700

    Add files via upload

---
+++ Poezdka.py
@@ -0,0 +1,9 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def odometer (int [] oksana):
+    kilom = 0
+    for i in range (N):
+        x.append (random.randint (1, 100))
+    for j in range (0, N, 2):
+        kilom += x [j] * x [j + 1]
+    return kilom
