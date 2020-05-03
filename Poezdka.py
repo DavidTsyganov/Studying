8da67881b8529f6ce0d6a9c76149e3856b756b95
DavidTsyganov <60532779+DavidTsyganov@users.noreply.github.com>
2020-05-04 00:30:39 +0700

    Update Poezdka.py

--- Poezdka.py
+++ Poezdka.py
@@ -1,6 +1,4 @@
-Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def odometer (int [] oksana):
+def odometer (int [] oksana):
     kilom = 0
     for i in range (N):
         x.append (random.randint (1, 100))
