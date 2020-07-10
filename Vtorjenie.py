3b7b37ab0674a311fde5e38381269c3030bbb5a5
DavidTsyganov <60532779+DavidTsyganov@users.noreply.github.com>
2020-07-10 23:44:54 +0700

    Update Vtorjenie.py

--- Vtorjenie.py
+++ Vtorjenie.py
@@ -6,31 +6,31 @@ def ConquestCampaign(N, M, L, battalion):
             b.append(0)  # Заполнение двумерного списка нулями.
         a.append(b)  # Добавление подсписков в основной список.
 
-    for i in range(len(battalion)): #Цикл, организующий высадку десантников по координатам. Точкам высадки присваивается 1.
+    for i in range(len(battalion)):  # Цикл, организующий высадку десантников по координатам. Точкам высадки присваивается 1.
         if i == 0 or i % 2 == 0:
             for k in range(len(a) + 1):
                 if k == battalion[i]:
                     for j in range(len(b) + 1):
                         if battalion[i + 1] == j:
                             a[k - 1][j - 1] = 1
-    for i in range(len(a)): #Цикл, увеличивающий на +1 координату высадки в первый день.
+    for i in range(len(a)):  # Цикл, увеличивающий на +1 координату высадки в первый день.
         for j in range(len(b)):
             if a[i][j] != 0:
                 a[i][j] += 1
-    for i in range(len(a)): #Проверка заполнения всех клеток.
+    for i in range(len(a)):  # Проверка заполнения всех клеток.
         for j in range(len(b)):
             if a[i][j] == 0:
-                proverka = False # При заполнении не всех клеток. Происходит дальнейший захват.
+                proverka = False  # При заполнении не всех клеток. Происходит дальнейший захват.
                 break
             else:
-                proverka = True # При заполнении всех клеток. День захвата - 1. Дальнейший захват не происходит.
+                proverka = True  # При заполнении всех клеток. День захвата - 1. Дальнейший захват не происходит.
 
-    if proverka == True:
+    if proverka:
         k = 1
     else:
         k = 0
-        while proverka == False: # Цикл выполняется пока есть незахваченные клетки.
-            for i in range(len(a)): # Проверка наличия свободных клеток.
+        while not proverka:  # Цикл выполняется пока есть незахваченные клетки.
+            for i in range(len(a)):  # Проверка наличия свободных клеток.
                 for j in range(len(b)):
                     if a[i][j] == 0:
                         proverka = False
@@ -39,7 +39,7 @@ def ConquestCampaign(N, M, L, battalion):
                         proverka = True
                         break
 
-            for i in range(len(a)): # Следующие циклы заполняют соседние клетки уже заполненных клеток.
+            for i in range(len(a)):  # Следующие циклы заполняют соседние клетки уже заполненных клеток.
                 for j in range(len(b)):
                     if a[i][j] != 0:
                         a[i][j] += 1
@@ -63,5 +63,5 @@ def ConquestCampaign(N, M, L, battalion):
                 for i in range(len(a) - 1, 0, -1):
                     if a[i][j] == 0 and a[i - 1][j] >= 2:
                         a[i][j] += 1
-            k += 1 # Подсчет количества дней, прошедших с момента начала операции захвата.
+            k += 1  # Подсчет количества дней, прошедших с момента начала операции захвата.
     return k
